0850258908f108e59ba2b5316da1b8997d941c06
kevin <kl.klee.k.lee@gmail.com>
2025-04-26 14:51:49 -0400

    Basic application finished for testing

---
+++ src/UserForm.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+
+
+const UserForm = ({onUserAdd}) => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+       onUserAdd({name,email})  
+      }
+
+return <form onSubmit={handleSubmit}>
+    <div>
+        <label>Name</label>
+        <input value={name} onChange={e => setName(e.target.value)}/>
+        <label>Email</label>
+        <input value={email} onChange={e => setEmail(e.target.value)}/>
+    </div>
+    <button> Add User</button>
+
+</form>
+}
+
+export default UserForm;
---
+++ src/UserForm.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+
+
+// imported from global environment
+test("")
